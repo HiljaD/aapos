38f6c6bd0162831ef91758d661ede13df1017ce9
Jari <jarirapani.turunen@gmail.com>
2023-06-05 13:53:50 +0300

    Cleaned Hello word!

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './HelloWord';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
